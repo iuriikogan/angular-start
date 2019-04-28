5b24c24d00cedf7c64a45583dab21634fd336475
Iurii Kogan <koganiurii@gmail.com>
2019-04-28 22:37:57 +0700

    compiles but doesn't render

--- APM-Start/src/app/todo/Shared/todo.service.ts
+++ APM-Start/src/app/todo/Shared/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 @Injectable()
 export class ToDoService {
