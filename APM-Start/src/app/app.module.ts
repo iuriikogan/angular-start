573dbdb50145d4381aea62453b6dfbb0be05a17e
Iurii Kogan <koganiurii@gmail.com>
2019-04-28 22:43:38 +0700

    ///

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { ToDoComponent } from './todo/todo.component';
-import { AngularFireModule } from 'angularfire2'
+import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
 @NgModule({
   declarations: [
