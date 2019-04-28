6a431b2b0347f31ce212d25adb8f47470b30fad3
Iurii Kogan <koganiurii@gmail.com>
2019-04-28 13:23:54 +0700

    something is broker

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: 'app.component.html' 
+  templateUrl: 'app.component.html'
 })
 
 export class AppComponent {
-  pageTitle: string = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.ts'
+})
+
+export class ProductListComponent { }
