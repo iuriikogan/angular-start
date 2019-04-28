8b27a20af72869e762093010f6c8de98001aa72d
Iurii Kogan <koganiurii@gmail.com>
2019-04-28 21:11:59 +0700

    //

--- APM-Start/src/app/todo/todo.component.ts
+++ APM-Start/src/app/todo/todo.component.ts
@@ -31,4 +31,8 @@ export class TodoComponent implements OnInit {
     this.toDoService.addTitle(itemTitle.value);
     itemTitle.value = null;
   }
+
+  alterCheck($key: string, isChecked) {
+    this.toDoService.checkOrUncheckTitle($key, !isChecked);
+  }
 }
